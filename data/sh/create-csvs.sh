823d21977b401291033ddccf639a156e52209d27
ryan-talus <rzimmerman@talusanalytics.com>
2024-01-10 13:31:33 -0300

    Correction: format as CSV

--- data/sh/create-csvs.sh
+++ data/sh/create-csvs.sh
@@ -1,6 +1,6 @@
 export PGPASSWORD="1234";
 for i in ./queries/*; do
     query_name=$(basename -- "$i")
-    psql -h localhost -U postgres -d tracking -f $i > ./csv/${query_name%.*}.csv
+    psql -h localhost -U postgres -d tracking --csv -f $i > ./csv/${query_name%.*}.csv
 done
 
