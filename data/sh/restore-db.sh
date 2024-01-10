23d6d08e35c5be6cb2ff5a3a6cb597eccd9eb37a
ryan-talus <rzimmerman@talusanalytics.com>
2024-01-10 15:38:38 -0300

    Add auto-download of database dump if possible

--- data/sh/restore-db.sh
+++ data/sh/restore-db.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+if ! command -v aws --version &> /dev/null
+then
+    echo "AWS CLI not found, please download database dump manually."
+else
+    aws s3 sync s3://tracking-db-dump-2024/ ./dumps/
+fi
 docker exec tracking-db mkdir -p /dumps
 docker cp ./dumps/dump-tracking tracking-db:/dumps/dump-tracking
 docker exec -e PGPASSWORD="1234" tracking-db psql -h localhost -U postgres -d tracking -c "DROP SCHEMA public CASCADE;"
