1e1c72b07a692466c7627eaacdb24545302b6904
ryan-talus <rzimmerman@talusanalytics.com>
2024-01-10 14:07:42 -0300

    Updating script to use the docker container instead of relying on the host for psql

--- data/sh/create-csvs.sh
+++ data/sh/create-csvs.sh
@@ -1,6 +1,9 @@
-export PGPASSWORD="1234";
+docker cp ./queries tracking-db:/queries
+docker exec tracking-db mkdir -p ./csv
 for i in ./queries/*; do
     query_name=$(basename -- "$i")
-    psql -h localhost -U postgres -d tracking --csv -f $i > ./csv/${query_name%.*}.csv
+    echo $query_name
+    docker exec tracking-db PGPASSWORD="1234" psql -h localhost -U postgres -d tracking --csv -f $i > ./csv/${query_name%.*}.csv
 done
+docker cp tracking-db:/csv/ .
 
