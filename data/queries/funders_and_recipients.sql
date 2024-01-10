1c86a6ab8f5755e437c050704239a7be2254d3ad
Hailey Robertson <65562768+haileyrobertson@users.noreply.github.com>
2024-01-10 16:41:49 -0500

    Funder and recipient query and csv

---
+++ data/queries/funders_and_recipients.sql
@@ -0,0 +1,43 @@
+with all_countries as(
+		-- All the stakeholders which are "countries"
+	SELECT id FROM stakeholders 
+		WHERE subcat = 'country' 
+		AND iso3 IS NOT null
+		AND "show"
+), all_orgs as(
+	-- All the organizations which should become pages
+	SELECT id FROM stakeholders 
+		JOIN children_to_parents_direct_credit 
+		ON stakeholders.id = children_to_parents_direct_credit.parent_id
+		WHERE stakeholders.cat != 'government' and stakeholders.subcat != 'sub-organization'
+		AND stakeholders.iso3 IS NULL
+		AND child_id = parent_id
+)
+-- All transaction pairs for selected stakeholders
+SELECT
+    s1.name AS "Funder",
+    s2.name AS "Recipient",
+    ROUND(SUM(sf.value)) AS "Total value"
+FROM
+    flows_to_stakeholder_origins_direct_credit ftsodc
+JOIN
+    simple_flows sf ON ftsodc.flow_id = sf.sf_id
+JOIN
+    stakeholders s1 ON ftsodc.stakeholder_id = s1.id
+JOIN
+    flows_to_stakeholder_targets_direct_credit ftstdc ON sf.sf_id = ftstdc.flow_id
+JOIN
+    stakeholders s2 ON ftstdc.stakeholder_id = s2.id
+WHERE 
+    sf.flow_type = 'disbursed_funds' 
+    AND sf."year" BETWEEN 2014 AND 3000
+    AND (
+    	s1.id in (select * from all_countries) or s1.id in (select * from all_orgs)
+    	)
+    AND (
+    	s2.id in (select * from all_countries) or s2.id in (select * from all_orgs)
+    	)
+GROUP BY
+    s1.name, s2.name
+ORDER BY
+    "Total value" DESC;
