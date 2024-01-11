e695be3a0905d7996884cc88d1e356aacfcd07ee
ryan-talus <rzimmerman@talusanalytics.com>
2024-01-11 13:56:51 -0300

    default color for the text

--- src/components/layout/Providers.tsx
+++ src/components/layout/Providers.tsx
@@ -18,7 +18,7 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     font-family: "Open Sans", Arial, Helvetica, sans-serif;
-    color: ${({ theme }) => theme.grayscaleBlack};
+    color: ${({ theme }) => theme.common.colors.textPrimary};
   }
   * {
     box-sizing: border-box;
