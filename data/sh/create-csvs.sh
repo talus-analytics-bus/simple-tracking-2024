5eeb0740d56c621b88fa73fc43f088fc467ab2cc
ryan-talus <rzimmerman@talusanalytics.com>
2024-01-12 12:39:41 -0300

    No longer need these data scripts

--- data/sh/create-csvs.sh
+++
@@ -1,9 +0,0 @@
-docker exec simple-tracking-db mkdir -p ./csv
-docker exec simple-tracking-db mkdir -p ./queries
-for i in ./queries/*; do
-    docker cp $i simple-tracking-db:$i
-    query_name=$(basename -- "$i")
-    docker exec -e PGPASSWORD="1234" simple-tracking-db psql -h localhost -U postgres -d tracking --csv -f $i > ./csv/${query_name%.*}.csv
-done
-docker cp simple-tracking-db:/csv/ .
-
