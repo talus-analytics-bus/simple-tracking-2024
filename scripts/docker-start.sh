b1075f02c3c70003341fce440949d0112f72e5d0
ryan-talus <rzimmerman@talusanalytics.com>
2024-01-12 12:40:18 -0300

    Adding auto-run queries and auto-update queries

--- scripts/docker-start.sh
+++ scripts/docker-start.sh
@@ -24,17 +24,31 @@ export AIRTABLE_API_KEY=$(
 
   # echo $AIRTABLE_SECRET | jq  -r .SecretString | jq -r .AIRTABLE_API_KEY\
 
-if [ "$reset" == "1" ]; then 
+if [ "$reset" == "1" ]; then
   bvm upgrade;
   yarn install;
   gatsby clean;
 fi
 
-if [ "$clean" == "1" ]; then 
+if [ "$clean" == "1" ]; then
   gatsby clean
 fi
 
 export INTERNAL_STATUS_PORT=8888
+
+for i in data/queries/*; do
+    query_name=$(basename -- "$i")
+    echo "Running Query $query_name"
+    psql -h database -U postgres -d tracking --csv -f $i > data/csv/${query_name%.*}.csv
+done
+
+{
+  inotifywait -e close_write,moved_to,create -m data/queries/ |
+  while read -r directory events filename; do
+    echo "Running Query $filename"
+    psql -h database -U postgres -d tracking --csv -f data/queries/$filename > data/csv/${filename%.*}.csv
+  done
+} &
 
 gatsby develop -H 0.0.0.0;
 
