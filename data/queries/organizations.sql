8f24f89fee70856fe00b131bd5850c3f17bc0a04
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2024-01-17 18:18:26 -0300

    Taking out organizations where 'show' is false

--- data/queries/organizations.sql
+++ data/queries/organizations.sql
@@ -1,8 +1,9 @@
 -- all the organizations which should become pages
-SELECT name FROM stakeholders 
+SELECT name FROM stakeholders
 	JOIN children_to_parents_direct_credit 
 	ON stakeholders.id = children_to_parents_direct_credit.parent_id
 	WHERE stakeholders.cat != 'government'
 	AND stakeholders.subcat != 'sub-organization'
 	AND stakeholders.iso3 IS NULL
 	AND child_id = parent_id
+	AND stakeholders.show
