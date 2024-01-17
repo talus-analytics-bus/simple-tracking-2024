d27c861737cca1512a3536f1c72ba8c770fad926
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2024-01-17 18:19:18 -0300

    Adding "stakeholders" query which combines countries and organizations

---
+++ data/queries/stakeholders.sql
@@ -0,0 +1,19 @@
+SELECT sta.name, sta.iso3
+	FROM stakeholders as sta
+	JOIN children_to_parents_direct_credit AS ctpdc
+		ON sta.id = ctpdc.parent_id
+	WHERE (
+		-- All the stakeholders which are "countries"
+		sta.subcat = 'country'
+		AND sta.iso3 IS NOT NULL
+		AND ctpdc.child_id = ctpdc.parent_id
+		AND sta.show
+	) OR (
+		-- All the stakeholders which are "organizations"
+		sta.cat != 'government'
+		AND sta.subcat != 'sub-organization'
+		AND ctpdc.child_id = ctpdc.parent_id
+		AND sta.iso3 IS NULL
+		AND sta.show
+	)
+
