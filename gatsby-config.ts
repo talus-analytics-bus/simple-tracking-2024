6b8c06800c1ca9d5adf9dfd42eb13c58ef04a504
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2024-02-16 00:53:57 -0300

    Take out unnecessary console log

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -82,7 +82,6 @@ const config: GatsbyConfig = {
         skipUndefinedUrls: true,
         prepareUrl: (url: string) => {
           if (!url || url === 'N/A' || url === 'AP') return undefined
-          console.log(`https://flags.talusanalytics.com/300px/${url.toLowerCase()}.png`)
           return `https://flags.talusanalytics.com/300px/${url.toLowerCase()}.png`
         },
       },
