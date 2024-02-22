724f7329d5110368812947436afbf058da2bde7d
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2024-02-22 19:52:01 -0300

    New Text Styles

--- src/figma/textStyles.ts
+++ src/figma/textStyles.ts
@@ -1,78 +1,144 @@
 export default {
+  textStyleBigLanding: `
+    font-size: 60px;
+    font-family: "Barlow Condensed";
+    font-weight: 500;
+    font-style: normal;
+    line-height: 72px;
+    text-decoration: none;
+    text-transform: none;
+  `,
   textStyleH1: `
-	font-size:36px;
-	font-family:"Rawline";
-	font-weight:bolder;
-	font-style:normal;
-	text-decoration:none;
-`,
+    font-size: 36px;
+    font-family: "Barlow Condensed";
+    font-weight: 500;
+    font-style: normal;
+    line-height: 42px;
+    text-decoration: none;
+    text-transform: none;
+  `,
   textStyleH2: `
-	font-size:30px;
-	font-family:"Rawline";
-	font-weight:bolder;
-	font-style:normal;
-	line-height:34px;
-	text-decoration:none;
-`,
+    font-size: 30px;
+    font-family: "Barlow Condensed";
+    font-weight: 500;
+    font-style: normal;
+    line-height: 36px;
+    text-decoration: none;
+    text-transform: none;
+  `,
   textStyleH3: `
-	font-size:24px;
-	font-family:"Rawline";
-	font-weight:bolder;
-	font-style:normal;
-	line-height:30px;
-	text-decoration:none;
-`,
-  textStyleAlertTitles: `
-	font-size:20px;
-	font-family:"Open Sans", Arial, Helvetica, sans-serif;
-	font-weight:bolder;
-	font-style:normal;
-	line-height:25px;
-	text-decoration:none;
-`,
+    font-size: 24px;
+    font-family: "Barlow Condensed";
+    font-weight: 500;
+    font-style: normal;
+    line-height: 29px;
+    text-decoration: none;
+    text-transform: none;
+  `,
+  textStyleH4: `
+    font-size: 22px;
+    font-family: "Open Sans";
+    font-weight: 600;
+    font-style: normal;
+    line-height: 30px;
+    text-decoration: none;
+    text-transform: none;
+  `,
+  textStyleBigParagraph: `
+    font-size: 20px;
+    font-family: "Open Sans";
+    font-weight: 400;
+    font-style: normal;
+    line-height: 27px;
+    text-decoration: none;
+    text-transform: none;
+  `,
+  textStyleBigParagraphSemibold: `
+    font-size: 20px;
+    font-family: "Open Sans";
+    font-weight: 600;
+    font-style: normal;
+    line-height: 27px;
+    text-decoration: none;
+    text-transform: none;
+  `,
   textStyleParagraph: `
-	font-size:16px;
-	font-family:"Open Sans", Arial, Helvetica, sans-serif;
-	font-weight:normal;
-	font-style:normal;
-	line-height:25px;
-	text-decoration:none;
-`,
+    font-size: 16px;
+    font-family: "Open Sans";
+    font-weight: 400;
+    font-style: normal;
+    line-height: 25px;
+    text-decoration: none;
+    text-transform: none;
+  `,
   textStyleParagraphSemibold: `
-	font-size:16px;
-	font-family:"Open Sans", Arial, Helvetica, sans-serif;
-	font-weight:bolder;
-	font-style:normal;
-	line-height:25px;
-	text-decoration:none;
-`,
-  textStyleParagraphBold: `
-	font-size:16px;
-	font-family:"Open Sans", Arial, Helvetica, sans-serif;
-	font-weight:bolder;
-	font-style:normal;
-	line-height:25px;
-	text-decoration:none;
-`,
-  textStyleSmallLabel: `
-	font-size:12px;
-	font-family:"Open Sans", Arial, Helvetica, sans-serif;
-	font-weight:normal;
-	font-style:normal;
-	text-decoration:none;
-`,
-  textStyleChartLabels: `
-	font-size:16px;
-	font-family:"Open Sans", Arial, Helvetica, sans-serif;
-	font-weight:normal;
-	font-style:normal;
-	text-decoration:none;
-`,
-  textStyleChartLabelsEmphasis: `
-	font-size:16px;
-	font-family:"Open Sans", Arial, Helvetica, sans-serif;
-	font-weight:bolder;
-	font-style:normal;
-	text-decoration:none;
-`,
+    font-size: 16px;
+    font-family: "Open Sans";
+    font-weight: 600;
+    font-style: normal;
+    line-height: 25px;
+    text-decoration: none;
+    text-transform: none;
+  `,
+  textStyleSmallParagraph: `
+    font-size: 14px;
+    font-family: "Open Sans";
+    font-weight: 400;
+    font-style: normal;
+    line-height: 21px;
+    text-decoration: none;
+    text-transform: none;
+  `,
+  textStyleTinyParagraph: `
+    font-size: 12px;
+    font-family: "Open Sans";
+    font-weight: 400;
+    font-style: normal;
+    line-height: 17px;
+    text-decoration: none;
+    text-transform: none;
+  `,
+  textStyleSmallNumbers: `
+    font-size: 14px;
+    font-family: "Fira Mono";
+    font-weight: 400;
+    font-style: normal;
+    line-height: 18px;
+    text-decoration: none;
+    text-transform: none;
+  `,
+  textStyleNumbers: `
+    font-size: 16px;
+    font-family: "Fira Mono";
+    font-weight: 400;
+    font-style: normal;
+    line-height: 25px;
+    text-decoration: none;
+    text-transform: none;
+  `,
+  textStyleSmallNumbersSemibold: `
+    font-size: 14px;
+    font-family: "Fira Mono";
+    font-weight: 500;
+    font-style: normal;
+    line-height: 18px;
+    text-decoration: none;
+    text-transform: none;
+  `,
+  textStyleBigNumber: `
+    font-size: 40px;
+    font-family: "Fira Mono";
+    font-weight: 500;
+    font-style: normal;
+    text-decoration: none;
+    text-transform: none;
+  `,
+  textStyleMedNumber: `
+    font-size: 40px;
+    font-family: "Fira Mono";
+    font-weight: 400;
+    font-style: normal;
+    text-decoration: none;
+    text-transform: none;
+  `,
 }
