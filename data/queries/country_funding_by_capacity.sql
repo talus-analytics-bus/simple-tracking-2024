d709b4c41ec17dac84bf3a1371fcc99a01e87925
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2024-02-26 15:03:43 -0300

    Writing out full headers for all the JEE categories

--- data/queries/country_funding_by_capacity.sql
+++ data/queries/country_funding_by_capacity.sql
@@ -27,25 +27,25 @@ LEFT JOIN (
 	)
 	SELECT
 		"Name",
-		MAX(CASE WHEN "Core Capacity" = 'P.1' THEN "Total received" END) AS "P1",
-		MAX(CASE WHEN "Core Capacity" = 'P.2' THEN "Total received" END) AS "P2",
-		MAX(CASE WHEN "Core Capacity" = 'P.3' THEN "Total received" END) AS "P3",
-		MAX(CASE WHEN "Core Capacity" = 'P.4' THEN "Total received" END) AS "P4",
-		MAX(CASE WHEN "Core Capacity" = 'P.5' THEN "Total received" END) AS "P5",
-		MAX(CASE WHEN "Core Capacity" = 'P.6' THEN "Total received" END) AS "P6",
-		MAX(CASE WHEN "Core Capacity" = 'P.7' THEN "Total received" END) AS "P7",
-		MAX(CASE WHEN "Core Capacity" = 'D.1' THEN "Total received" END) AS "D1",
-		MAX(CASE WHEN "Core Capacity" = 'D.2' THEN "Total received" END) AS "D2",
-		MAX(CASE WHEN "Core Capacity" = 'D.3' THEN "Total received" END) AS "D3",
-		MAX(CASE WHEN "Core Capacity" = 'D.4' THEN "Total received" END) AS "D4",
-		MAX(CASE WHEN "Core Capacity" = 'R.1' THEN "Total received" END) AS "R1",
-		MAX(CASE WHEN "Core Capacity" = 'R.2' THEN "Total received" END) AS "R2",
-		MAX(CASE WHEN "Core Capacity" = 'R.3' THEN "Total received" END) AS "R3",
-		MAX(CASE WHEN "Core Capacity" = 'R.4' THEN "Total received" END) AS "R4",
-		MAX(CASE WHEN "Core Capacity" = 'R.5' THEN "Total received" END) AS "R5",
-		MAX(CASE WHEN "Core Capacity" = 'RE' THEN "Total received" END) AS "RE",
-		MAX(CASE WHEN "Core Capacity" = 'PoE' THEN "Total received" END) AS "PoE",
-		MAX(CASE WHEN "Core Capacity" = 'CE' THEN "Total received" END) AS "CE"
+		MAX(CASE WHEN "Core Capacity" = 'P.1' THEN "Total received" END) AS "National legislation, policy and financing",
+		MAX(CASE WHEN "Core Capacity" = 'P.2' THEN "Total received" END) AS "IHR coordination, communication and advocacy",
+		MAX(CASE WHEN "Core Capacity" = 'P.3' THEN "Total received" END) AS "Antimicrobial resistance (AMR)",
+		MAX(CASE WHEN "Core Capacity" = 'P.4' THEN "Total received" END) AS "Zoonotic disease",
+		MAX(CASE WHEN "Core Capacity" = 'P.5' THEN "Total received" END) AS "Food safety",
+		MAX(CASE WHEN "Core Capacity" = 'P.6' THEN "Total received" END) AS "Biosafety and biosecurity",
+		MAX(CASE WHEN "Core Capacity" = 'P.7' THEN "Total received" END) AS "Immunization",
+		MAX(CASE WHEN "Core Capacity" = 'D.1' THEN "Total received" END) AS "National laboratory system",
+		MAX(CASE WHEN "Core Capacity" = 'D.2' THEN "Total received" END) AS "Real-time surveillance",
+		MAX(CASE WHEN "Core Capacity" = 'D.3' THEN "Total received" END) AS "Reporting",
+		MAX(CASE WHEN "Core Capacity" = 'D.4' THEN "Total received" END) AS "Workforce development",
+		MAX(CASE WHEN "Core Capacity" = 'R.1' THEN "Total received" END) AS "Preparedness",
+		MAX(CASE WHEN "Core Capacity" = 'R.2' THEN "Total received" END) AS "Emergency response operations",
+		MAX(CASE WHEN "Core Capacity" = 'R.3' THEN "Total received" END) AS "Linking public health and security authorities",
+		MAX(CASE WHEN "Core Capacity" = 'R.4' THEN "Total received" END) AS "Medical countermeasures and personnel deployment",
+		MAX(CASE WHEN "Core Capacity" = 'R.5' THEN "Total received" END) AS "Risk communication",
+		MAX(CASE WHEN "Core Capacity" = 'RE' THEN "Total received" END) AS "Radiation events",
+		MAX(CASE WHEN "Core Capacity" = 'PoE' THEN "Total received" END) AS "Points of entry",
+		MAX(CASE WHEN "Core Capacity" = 'CE' THEN "Total received" END) AS "Chemical events"
 	FROM core_capacity_sums
 	GROUP BY
 		"Name"
@@ -55,49 +55,49 @@ LEFT JOIN (
 ON all_countries.name = received_capacities."Name"
 LEFT JOIN (
 	-- all the countries which disbursed core capacity funding
-		WITH core_capacity_sums AS (
-			SELECT
-				s.name AS "Name",
-				cc.name AS "Core Capacity",
-				ROUND(SUM(sf.value)) AS "Total disbursed"
-			FROM
-				simple_flows sf
-				JOIN flows_to_stakeholder_targets_direct_credit ftsodc ON sf.sf_id = ftsodc.flow_id
-				JOIN stakeholders s ON s.id = ftsodc.stakeholder_id
-				JOIN ccs_to_flows ctf ON ctf.flow_id = sf.sf_id
-				JOIN core_capacities cc ON cc.id = ctf.cc_id
-			WHERE
-				sf.flow_type = 'disbursed_funds'
-				AND sf.year BETWEEN 2014 AND 2022
-			GROUP BY
-				s.name,
-				cc.name
-		)
+	WITH core_capacity_sums AS (
 		SELECT
-			"Name",
-			MAX(CASE WHEN "Core Capacity" = 'P.1' THEN "Total disbursed" END) AS "P1",
-			MAX(CASE WHEN "Core Capacity" = 'P.2' THEN "Total disbursed" END) AS "P2",
-			MAX(CASE WHEN "Core Capacity" = 'P.3' THEN "Total disbursed" END) AS "P3",
-			MAX(CASE WHEN "Core Capacity" = 'P.4' THEN "Total disbursed" END) AS "P4",
-			MAX(CASE WHEN "Core Capacity" = 'P.5' THEN "Total disbursed" END) AS "P5",
-			MAX(CASE WHEN "Core Capacity" = 'P.6' THEN "Total disbursed" END) AS "P6",
-			MAX(CASE WHEN "Core Capacity" = 'P.7' THEN "Total disbursed" END) AS "P7",
-			MAX(CASE WHEN "Core Capacity" = 'D.1' THEN "Total disbursed" END) AS "D1",
-			MAX(CASE WHEN "Core Capacity" = 'D.2' THEN "Total disbursed" END) AS "D2",
-			MAX(CASE WHEN "Core Capacity" = 'D.3' THEN "Total disbursed" END) AS "D3",
-			MAX(CASE WHEN "Core Capacity" = 'D.4' THEN "Total disbursed" END) AS "D4",
-			MAX(CASE WHEN "Core Capacity" = 'R.1' THEN "Total disbursed" END) AS "R1",
-			MAX(CASE WHEN "Core Capacity" = 'R.2' THEN "Total disbursed" END) AS "R2",
-			MAX(CASE WHEN "Core Capacity" = 'R.3' THEN "Total disbursed" END) AS "R3",
-			MAX(CASE WHEN "Core Capacity" = 'R.4' THEN "Total disbursed" END) AS "R4",
-			MAX(CASE WHEN "Core Capacity" = 'R.5' THEN "Total disbursed" END) AS "R5",
-			MAX(CASE WHEN "Core Capacity" = 'RE' THEN "Total disbursed" END) AS "RE",
-			MAX(CASE WHEN "Core Capacity" = 'PoE' THEN "Total disbursed" END) AS "PoE",
-			MAX(CASE WHEN "Core Capacity" = 'CE' THEN "Total disbursed" END) AS "CE"
-		FROM core_capacity_sums
+			s.name AS "Name",
+			cc.name AS "Core Capacity",
+			ROUND(SUM(sf.value)) AS "Total disbursed"
+		FROM
+			simple_flows sf
+			JOIN flows_to_stakeholder_targets_direct_credit ftsodc ON sf.sf_id = ftsodc.flow_id
+			JOIN stakeholders s ON s.id = ftsodc.stakeholder_id
+			JOIN ccs_to_flows ctf ON ctf.flow_id = sf.sf_id
+			JOIN core_capacities cc ON cc.id = ctf.cc_id
+		WHERE
+			sf.flow_type = 'disbursed_funds'
+			AND sf.year BETWEEN 2014 AND 2022
 		GROUP BY
-			"Name"
-		ORDER BY
-			"Name"
+			s.name,
+			cc.name
+	)
+	SELECT
+		"Name",
+		MAX(CASE WHEN "Core Capacity" = 'P.1' THEN "Total disbursed" END) AS "National legislation, policy and financing",
+		MAX(CASE WHEN "Core Capacity" = 'P.2' THEN "Total disbursed" END) AS "IHR coordination, communication and advocacy",
+		MAX(CASE WHEN "Core Capacity" = 'P.3' THEN "Total disbursed" END) AS "Antimicrobial resistance (AMR)",
+		MAX(CASE WHEN "Core Capacity" = 'P.4' THEN "Total disbursed" END) AS "Zoonotic disease",
+		MAX(CASE WHEN "Core Capacity" = 'P.5' THEN "Total disbursed" END) AS "Food safety",
+		MAX(CASE WHEN "Core Capacity" = 'P.6' THEN "Total disbursed" END) AS "Biosafety and biosecurity",
+		MAX(CASE WHEN "Core Capacity" = 'P.7' THEN "Total disbursed" END) AS "Immunization",
+		MAX(CASE WHEN "Core Capacity" = 'D.1' THEN "Total disbursed" END) AS "National laboratory system",
+		MAX(CASE WHEN "Core Capacity" = 'D.2' THEN "Total disbursed" END) AS "Real-time surveillance",
+		MAX(CASE WHEN "Core Capacity" = 'D.3' THEN "Total disbursed" END) AS "Reporting",
+		MAX(CASE WHEN "Core Capacity" = 'D.4' THEN "Total disbursed" END) AS "Workforce development",
+		MAX(CASE WHEN "Core Capacity" = 'R.1' THEN "Total disbursed" END) AS "Preparedness",
+		MAX(CASE WHEN "Core Capacity" = 'R.2' THEN "Total disbursed" END) AS "Emergency response operations",
+		MAX(CASE WHEN "Core Capacity" = 'R.3' THEN "Total disbursed" END) AS "Linking public health and security authorities",
+		MAX(CASE WHEN "Core Capacity" = 'R.4' THEN "Total disbursed" END) AS "Medical countermeasures and personnel deployment",
+		MAX(CASE WHEN "Core Capacity" = 'R.5' THEN "Total disbursed" END) AS "Risk communication",
+		MAX(CASE WHEN "Core Capacity" = 'RE' THEN "Total disbursed" END) AS "Radiation events",
+		MAX(CASE WHEN "Core Capacity" = 'PoE' THEN "Total disbursed" END) AS "Points of entry",
+		MAX(CASE WHEN "Core Capacity" = 'CE' THEN "Total disbursed" END) AS "Chemical events"
+	FROM core_capacity_sums
+	GROUP BY
+		"Name"
+	ORDER BY
+		"Name"
 ) disbursed_capacities
 ON all_countries.name = disbursed_capacities."Name"
