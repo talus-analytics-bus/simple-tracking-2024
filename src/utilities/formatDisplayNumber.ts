d5607025cd44eff169985bd03cb8a78f217faa11
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2024-02-29 19:41:43 -0300

    playing around with ideas for creating nice round pretty numbers

--- src/utilities/formatDisplayNumber.ts
+++ src/utilities/formatDisplayNumber.ts
@@ -5,6 +5,21 @@ const format = Intl.NumberFormat('en-US', {
   notation: 'compact',
 })
 
-const formatDisplayNumber = (num: number) => format.format(num)
+const formatDisplayNumber = (num: number) => {
+  if (num < 1000) return num
+
+  const tier = (Math.log10(num) / 3) | 0
+  const scale = Math.pow(10, tier * 3)
+  const scaled = num / scale
+
+  let round = scaled
+  if (scaled < 10) round = (Math.round((scaled * 10) / 5) * 5) / 10
+  else if (scaled > 100) round = Math.round(scaled / 50) * 50
+  else round = Math.round(scaled)
+
+  return format.format(round * scale)
+}
+
+// const formatDisplayNumber = (num: number) => format.format(num)
 
 export default formatDisplayNumber
