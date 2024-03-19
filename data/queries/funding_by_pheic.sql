da65b68326ac9df476817fd9f96d7bbe62960731
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2024-03-19 12:49:02 -0300

    Refactoring funding by pheic to meet new stakeholder definition

--- data/queries/funding_by_pheic.sql
+++ data/queries/funding_by_pheic.sql
@@ -1,26 +1,30 @@
-WITH all_countries AS (
-    -- All the stakeholders which are "countries"
-    SELECT id FROM stakeholders 
-    WHERE subcat = 'country' 
-    AND iso3 IS NOT NULL
-    AND "show"
-), 
-all_orgs AS (
-    -- All the organizations which should become pages
-    SELECT id FROM stakeholders 
-    JOIN children_to_parents_direct_credit ON stakeholders.id = children_to_parents_direct_credit.parent_id
-    WHERE stakeholders.cat != 'government' 
-    AND stakeholders.subcat != 'sub-organization'
-    AND stakeholders.iso3 IS NULL
-    AND child_id = parent_id
+WITH top_level_stakeholders AS (
+	SELECT id
+	FROM stakeholders
+	INNER JOIN LATERAL (
+		SELECT COUNT(id) > 1 as is_sub_stakeholder
+		FROM stakeholders sta
+		JOIN children_to_parents_direct_credit ctpdc
+			ON sta.id = ctpdc.parent_id
+		WHERE ctpdc.child_id = stakeholders.id
+	) AS sub_stakeholders on True
+	WHERE
+		stakeholders.show
+		AND NOT sub_stakeholders.is_sub_stakeholder
+		AND stakeholders.slug != 'not-reported'
+		AND stakeholders.subcat NOT IN (
+			'sub-organization',
+			'region',
+			'agency'
+		)
 ),
-pheic_sums_received AS (
+received AS (
     -- all the countries which received response funding
     SELECT
         s.name AS name,
         e.name AS pheic,
-		sf.year AS year,
-        ROUND(SUM(sf.value)) AS received
+	sf.year AS year,
+        ROUND(SUM(sf.value)) AS total
     FROM
         simple_flows sf
         JOIN flows_to_stakeholder_targets_direct_credit ftstdc ON sf.sf_id = ftstdc.flow_id
@@ -30,19 +34,16 @@ pheic_sums_received AS (
     WHERE
         sf.flow_type = 'disbursed_funds'
         AND sf.response_or_capacity = 'response'
-        AND (s.id IN (SELECT * FROM all_countries) OR s.id IN (SELECT * FROM all_orgs))
-    GROUP BY
-        s.name,
-		sf.year,
-        e.name
+        AND s.id IN (SELECT * FROM top_level_stakeholders)
+    GROUP BY s.name, sf.year, e.name
 ),
-pheic_sums_disbursed AS (
+disbursed AS (
     -- all the countries which disbursed response funding
     SELECT
         s.name AS name,
         e.name AS pheic,
-		sf.year AS year,
-        ROUND(SUM(sf.value)) AS disbursed
+	sf.year AS year,
+        ROUND(SUM(sf.value)) AS total
     FROM
         simple_flows sf
         JOIN flows_to_stakeholder_origins_direct_credit ftsodc ON sf.sf_id = ftsodc.flow_id
@@ -52,22 +53,16 @@ pheic_sums_disbursed AS (
     WHERE
         sf.flow_type = 'disbursed_funds'
         AND sf.response_or_capacity = 'response'
-        AND (s.id IN (SELECT * FROM all_countries) OR s.id IN (SELECT * FROM all_orgs))
-    GROUP BY
-        s.name,
-		sf.year,
-        e.name
+        AND s.id IN (SELECT * FROM top_level_stakeholders)
+    GROUP BY s.name, sf.year, e.name
 )
 SELECT
-    COALESCE(pr.name, pd.name) AS name,
-	COALESCE(pr.year, pd.year) AS year,
-    COALESCE(pr.pheic, pd.pheic) AS pheic,
-    pr.received,
-    pd.disbursed
-FROM
-    pheic_sums_received pr
-FULL JOIN
-    pheic_sums_disbursed pd
-ON
-    pr.name = pd.name AND pr.pheic = pd.pheic
-order by pr.name, year DESC, pheic, received, disbursed;
+    COALESCE(received.name, disbursed.name) AS name,
+    COALESCE(received.year, disbursed.year) AS year,
+    COALESCE(received.pheic, disbursed.pheic) AS pheic,
+    received.total AS received,
+    disbursed.total AS disbursed
+FROM received
+FULL JOIN disbursed
+    ON received.name = disbursed.name AND received.pheic = disbursed.pheic
+order by name, year DESC, pheic, received, disbursed;
