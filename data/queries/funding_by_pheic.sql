dce9dd8aa287007937ef2a9bfeb46ad9a6d8c506
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2024-03-19 12:50:55 -0300

    Fixing indentation

--- data/queries/funding_by_pheic.sql
+++ data/queries/funding_by_pheic.sql
@@ -19,50 +19,50 @@ WITH top_level_stakeholders AS (
 		)
 ),
 received AS (
-    -- all the countries which received response funding
-    SELECT
-        s.name AS name,
-        e.name AS pheic,
+	-- all the countries which received response funding
+	SELECT
+		s.name AS name,
+		e.name AS pheic,
 	sf.year AS year,
-        ROUND(SUM(sf.value)) AS total
-    FROM
-        simple_flows sf
-        JOIN flows_to_stakeholder_targets_direct_credit ftstdc ON sf.sf_id = ftstdc.flow_id
-        JOIN stakeholders s ON s.id = ftstdc.stakeholder_id
-        JOIN events_to_flows etf ON etf.flow_id = sf.sf_id
-        JOIN events e ON e.id = etf.event_id
-    WHERE
-        sf.flow_type = 'disbursed_funds'
-        AND sf.response_or_capacity = 'response'
-        AND s.id IN (SELECT * FROM top_level_stakeholders)
-    GROUP BY s.name, sf.year, e.name
+		ROUND(SUM(sf.value)) AS total
+	FROM
+		simple_flows sf
+		JOIN flows_to_stakeholder_targets_direct_credit ftstdc ON sf.sf_id = ftstdc.flow_id
+		JOIN stakeholders s ON s.id = ftstdc.stakeholder_id
+		JOIN events_to_flows etf ON etf.flow_id = sf.sf_id
+		JOIN events e ON e.id = etf.event_id
+	WHERE
+		sf.flow_type = 'disbursed_funds'
+		AND sf.response_or_capacity = 'response'
+		AND s.id IN (SELECT * FROM top_level_stakeholders)
+	GROUP BY s.name, sf.year, e.name
 ),
 disbursed AS (
-    -- all the countries which disbursed response funding
-    SELECT
-        s.name AS name,
-        e.name AS pheic,
+	-- all the countries which disbursed response funding
+	SELECT
+		s.name AS name,
+		e.name AS pheic,
 	sf.year AS year,
-        ROUND(SUM(sf.value)) AS total
-    FROM
-        simple_flows sf
-        JOIN flows_to_stakeholder_origins_direct_credit ftsodc ON sf.sf_id = ftsodc.flow_id
-        JOIN stakeholders s ON s.id = ftsodc.stakeholder_id
-        JOIN events_to_flows etf ON etf.flow_id = sf.sf_id
-        JOIN events e ON e.id = etf.event_id
-    WHERE
-        sf.flow_type = 'disbursed_funds'
-        AND sf.response_or_capacity = 'response'
-        AND s.id IN (SELECT * FROM top_level_stakeholders)
-    GROUP BY s.name, sf.year, e.name
+		ROUND(SUM(sf.value)) AS total
+	FROM
+		simple_flows sf
+		JOIN flows_to_stakeholder_origins_direct_credit ftsodc ON sf.sf_id = ftsodc.flow_id
+		JOIN stakeholders s ON s.id = ftsodc.stakeholder_id
+		JOIN events_to_flows etf ON etf.flow_id = sf.sf_id
+		JOIN events e ON e.id = etf.event_id
+	WHERE
+		sf.flow_type = 'disbursed_funds'
+		AND sf.response_or_capacity = 'response'
+		AND s.id IN (SELECT * FROM top_level_stakeholders)
+	GROUP BY s.name, sf.year, e.name
 )
 SELECT
-    COALESCE(received.name, disbursed.name) AS name,
-    COALESCE(received.year, disbursed.year) AS year,
-    COALESCE(received.pheic, disbursed.pheic) AS pheic,
-    received.total AS received,
-    disbursed.total AS disbursed
+	COALESCE(received.name, disbursed.name) AS name,
+	COALESCE(received.year, disbursed.year) AS year,
+	COALESCE(received.pheic, disbursed.pheic) AS pheic,
+	received.total AS received,
+	disbursed.total AS disbursed
 FROM received
 FULL JOIN disbursed
-    ON received.name = disbursed.name AND received.pheic = disbursed.pheic
-order by name, year DESC, pheic, received, disbursed;
+	ON received.name = disbursed.name AND received.pheic = disbursed.pheic
+ORDER BY name, year DESC, pheic, received, disbursed;
