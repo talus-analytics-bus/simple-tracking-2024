b02cfeace63edbf69ad04404c0e7f11aaf025a68
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2024-03-21 14:20:46 -0300

    Adding About pages placeholders with navigation

---
+++ src/components/about/AboutNav.tsx
@@ -0,0 +1,96 @@
+import React from 'react'
+import styled, { useTheme } from 'styled-components'
+
+import { Link } from 'gatsby'
+
+const LinkList = styled.ul`
+  list-style: none;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 40px;
+  margin: 0;
+  margin-top: 10px;
+  padding: 0 0 30px 0;
+  border-bottom: 3px solid ${({ theme }) => theme.common.colors.surfaceGray100};
+`
+const PaddedLink = styled(Link)`
+  ${({ theme }) => theme.textStyleBigParagraph};
+  padding: 8px 20px 10px 20px;
+  background-color: white;
+  border-radius: 10px;
+  transition: 150ms ease;
+  color: ${({ theme }) => theme.common.colors.textPrimary};
+
+  &:hover {
+    transition: 250ms ease;
+    background-color: ${({ theme }) => theme.common.colors.surfaceThemeHover};
+    color: ${({ theme }) => theme.common.colors.textInvert};
+  }
+`
+
+const AboutNav = (): JSX.Element => {
+  const theme = useTheme()
+
+  return (
+    <LinkList>
+      <li>
+        <PaddedLink
+          activeStyle={{
+            background: theme.common.colors.surfaceThemeDarker,
+            color: theme.common.colors.textInvert,
+          }}
+          to="/about/overview/"
+        >
+          Overview
+        </PaddedLink>
+      </li>
+      <li>
+        <PaddedLink
+          activeStyle={{
+            background: theme.common.colors.surfaceThemeDarker,
+            color: theme.common.colors.textInvert,
+          }}
+          to="/about/methods/"
+        >
+          Methods
+        </PaddedLink>
+      </li>
+      <li>
+        <PaddedLink
+          activeStyle={{
+            background: theme.common.colors.surfaceThemeDarker,
+            color: theme.common.colors.textInvert,
+          }}
+          to="/about/data-sources/"
+        >
+          Data sources
+        </PaddedLink>
+      </li>
+      <li>
+        <PaddedLink
+          activeStyle={{
+            background: theme.common.colors.surfaceThemeDarker,
+            color: theme.common.colors.textInvert,
+          }}
+          to="/about/data-access/"
+        >
+          Data access
+        </PaddedLink>
+      </li>
+      <li>
+        <PaddedLink
+          activeStyle={{
+            background: theme.common.colors.surfaceThemeDarker,
+            color: theme.common.colors.textInvert,
+          }}
+          to="/about/publications/"
+        >
+          Publications
+        </PaddedLink>
+      </li>
+    </LinkList>
+  )
+}
+export default AboutNav
---
+++ src/pages/about/data-sources.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+import CMS from 'components/library/airtable-cms/'
+
+import Main from 'components/layout/Main'
+import NavBar from 'components/layout/NavBar/NavBar'
+import AboutNav from 'components/about/AboutNav'
+import Providers from 'components/layout/Providers'
+
+import AboutStyle from 'components/about/AboutStyle'
+import Footer from 'components/layout/Footer'
+// import useUserGuidePageData from 'cmsHooks/useUserGuideQuery'
+
+const DataSourcesPage = (): JSX.Element => {
+  // const cmsData = useUserGuidePageData()
+  const cmsData = {}
+  return (
+    <Providers>
+      <CMS.SEO
+        title="GHSS Tracking Data Sources"
+        description="Data Sources of the GHSS Tracking project"
+      />
+      <NavBar />
+      <Main style={{ maxWidth: 1000 }}>
+        <AboutNav />
+        <AboutStyle>
+          <h1>
+            {
+              // <CMS.Text name="H1" data={cmsData} />
+            }
+            DATA SOURCES PLACEHOLDER
+          </h1>
+          {
+            // <CMS.RichText name="User guide text" data={cmsData} />
+          }
+          PLACEHOLDER
+        </AboutStyle>
+      </Main>
+      <Footer />
+    </Providers>
+  )
+}
+
+export default DataSourcesPage
