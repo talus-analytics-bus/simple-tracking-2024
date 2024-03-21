c197381b1602b9bcf80f09d99e7fb81d8926f4b1
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2024-03-21 19:37:11 -0300

    Data sources pages

--- src/pages/about/data-sources.tsx
+++ src/pages/about/data-sources.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { PageProps, graphql } from 'gatsby'
 
 import CMS from 'components/library/airtable-cms/'
 
@@ -9,15 +10,61 @@ import Providers from 'components/layout/Providers'
 
 import AboutStyle from 'components/about/AboutStyle'
 import Footer from 'components/layout/Footer'
-// import useUserGuidePageData from 'cmsHooks/useUserGuideQuery'
 
-const DataSourcesPage = (): JSX.Element => {
-  // const cmsData = useUserGuidePageData()
-  const cmsData = {}
+import useAboutDataSourcesPageData from 'cmsHooks/useAboutDataSourcesPageData'
+import styled from 'styled-components'
+
+type Writeable<T> = { -readonly [P in keyof T]: T[P] }
+
+const DataSourcesTable = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 30px;
+
+  th {
+    text-align: left;
+    padding: 20px;
+    color: ${({ theme }) => theme.common.colors.textInvert};
+    background-color: ${({ theme }) => theme.common.colors.surfaceThemeDarker};
+  }
+
+  th:first-child {
+    width: 30%;
+  }
+
+  td {
+    padding: 20px;
+    ${({ theme }) => theme.textStyleParagraph};
+
+    a {
+      color: ${({ theme }) => theme.common.colors.textPrimary};
+    }
+  }
+
+  td:first-child {
+    ${({ theme }) => theme.textStyleParagraphSemibold};
+  }
+
+  tr:nth-child(odd) {
+    background-color: ${({ theme }) => theme.common.colors.surfaceGray50};
+  }
+`
+
+const DataSourcesPage = ({
+  data,
+}: PageProps<Queries.DataSourcesPageQuery>): JSX.Element => {
+  const cmsData = useAboutDataSourcesPageData()
+
+  const dataSources = (
+    data.dataSources.nodes as Writeable<
+      Queries.DataSourcesPageQuery['dataSources']['nodes']
+    >
+  ).sort((a, b) => a.source?.Order ?? 0 - (b.source?.Order ?? 0))
+
   return (
     <Providers>
       <CMS.SEO
-        title="GHSS Tracking Data Sources"
+        title={'GHSS Tracking' + CMS.getText(cmsData, 'H1 title')}
         description="Data Sources of the GHSS Tracking project"
       />
       <NavBar />
@@ -25,20 +72,48 @@ const DataSourcesPage = (): JSX.Element => {
         <AboutNav />
         <AboutStyle>
           <h1>
-            {
-              // <CMS.Text name="H1" data={cmsData} />
-            }
-            DATA SOURCES PLACEHOLDER
+            <CMS.Text name="H1 title" data={cmsData} />
           </h1>
-          {
-            // <CMS.RichText name="User guide text" data={cmsData} />
-          }
-          PLACEHOLDER
+          <CMS.RichText name="H1 paragraph" data={cmsData} />
         </AboutStyle>
+        <DataSourcesTable>
+          <thead>
+            <tr>
+              <th>Data Source</th>
+              <th>Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            {dataSources.map(node => (
+              <tr key={node.source?.Data_source}>
+                <td>{node.source?.Data_source}</td>
+                <td
+                  dangerouslySetInnerHTML={{
+                    __html: CMS.parseRichText(node.source?.Description ?? ''),
+                  }}
+                />
+              </tr>
+            ))}
+          </tbody>
+        </DataSourcesTable>
       </Main>
       <Footer />
     </Providers>
   )
 }
+
+export const query = graphql`
+  query DataSourcesPage {
+    dataSources: allAirtable(filter: { table: { eq: "Data sources" } }) {
+      nodes {
+        source: data {
+          Data_source
+          Description
+          Order
+        }
+      }
+    }
+  }
+`
 
 export default DataSourcesPage
