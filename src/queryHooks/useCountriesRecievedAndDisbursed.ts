8339b15f7b180a83dbc9b21783e322881b36363e
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2024-03-21 14:40:42 -0300

    Temporarily taking out the query for countries because we need a new CSV for this

--- src/queryHooks/useCountriesRecievedAndDisbursed.ts
+++ src/queryHooks/useCountriesRecievedAndDisbursed.ts
@@ -1,21 +1,23 @@
 import { graphql, useStaticQuery } from 'gatsby'
 
 const useCountriesReceivedAndDisbursed = () => {
-  const {
-    receivedAndDisbursed: { countries },
-  } = useStaticQuery<Queries.CountriesReceivedAndDisbursedQuery>(graphql`
-    query CountriesReceivedAndDisbursed {
-      receivedAndDisbursed: allReceivedAndDisbursedCsv(
-        filter: { iso3: { ne: "" } }
-      ) {
-        countries: nodes {
-          iso3
-          totalDisbursed
-          totalDisbursedReceived
-        }
-      }
-    }
-  `)
+  // const {
+  //   receivedAndDisbursed: { countries },
+  // } = useStaticQuery<Queries.CountriesReceivedAndDisbursedQuery>(graphql`
+  //   query CountriesReceivedAndDisbursed {
+  //     receivedAndDisbursed: allReceivedAndDisbursedCsv(
+  //       filter: { iso3: { ne: "" } }
+  //     ) {
+  //       countries: nodes {
+  //         iso3
+  //         totalDisbursed
+  //         totalDisbursedReceived
+  //       }
+  //     }
+  //   }
+  // `)
+  //
+  const countries = []
 
   return countries
 }
