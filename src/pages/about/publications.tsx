dc83f49e3b0e4ce81bda1fc6b300ade6f7da9755
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2024-03-22 17:00:08 -0300

    Adding citations from GHSS Idea landing page

--- src/pages/about/publications.tsx
+++ src/pages/about/publications.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import styled from 'styled-components'
+import { Page, PageProps, graphql } from 'gatsby'
 
 import CMS from 'components/library/airtable-cms/'
 
@@ -12,7 +14,53 @@ import Footer from 'components/layout/Footer'
 
 import useAboutPublicationsPageData from 'cmsHooks/useAboutPublicationsPageData'
 
-const PublicationsPage = (): JSX.Element => {
+const CitationText = styled.p`
+  line-height: 26px;
+  ${({ theme }) => theme.textStyleParagraph}
+
+  > a {
+    color: ${({ theme }) => theme.common.colors.textPrimary};
+  }
+`
+const TitleLink = styled.a`
+  ${({ theme }) => theme.textStyleParagraphSemibold};
+
+  &:hover {
+    text-decoration: underline;
+  }
+`
+const DOILink = styled.a`
+  color: ${({ theme }) => theme.common.colors.textPrimary};
+`
+
+const Citation = ({
+  data,
+}: PageProps<Queries.PublicationsPageQuery>['data']['publicationsByOurTeam']['nodes'][0]) => (
+  <CitationText>
+    {data?.Authors?.trim()}.{' '}
+    <TitleLink href={data?.URL ?? ''}>
+      {data?.Publication_Title?.trim()}.
+    </TitleLink>{' '}
+    {data?.Publication?.trim()}
+    {'. '}
+    {data?.Publication_Date &&
+      new Date(data?.Publication_Date).toLocaleDateString(undefined, {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      })}
+    {data?.DOI && (
+      <>
+        {'. '}
+        <DOILink href={data?.DOI}>{data?.DOI.trim()}</DOILink>
+      </>
+    )}
+  </CitationText>
+)
+
+const PublicationsPage = ({
+  data,
+}: PageProps<Queries.PublicationsPageQuery>): JSX.Element => {
   const cmsData = useAboutPublicationsPageData()
 
   return (
@@ -32,14 +80,77 @@ const PublicationsPage = (): JSX.Element => {
           <h2>
             <CMS.Text name="H2 title" data={cmsData} />
           </h2>
+          {data.publicationsByOurTeam.nodes.map(({ data }) => (
+            <Citation data={data} />
+          ))}
           <h2>
             <CMS.Text name="H3 title" data={cmsData} />
           </h2>
+          {data.publicationsCitingOurWork.nodes.map(({ data }) => (
+            <Citation data={data} />
+          ))}
         </AboutStyle>
       </Main>
       <Footer />
     </Providers>
   )
 }
+
+export const query = graphql`
+  query PublicationsPage {
+    publicationsByOurTeam: allAirtableCitations(
+      filter: {
+        data: {
+          Project: {
+            elemMatch: {
+              data: { Name: { eq: "Global Health Security Tracking" } }
+            }
+          }
+          NEW_citation_type: { ne: "By our team" }
+        }
+        table: { eq: "Citations" }
+      }
+    ) {
+      nodes {
+        data {
+          NEW_citation_type
+          Publication_Title
+          Authors
+          Publication_Year
+          Publication_Date
+          Publication
+          URL
+          DOI
+        }
+      }
+    }
+    publicationsCitingOurWork: allAirtableCitations(
+      filter: {
+        data: {
+          Project: {
+            elemMatch: {
+              data: { Name: { eq: "Global Health Security Tracking" } }
+            }
+          }
+          NEW_citation_type: { ne: "By our team" }
+        }
+        table: { eq: "Citations" }
+      }
+    ) {
+      nodes {
+        data {
+          NEW_citation_type
+          Publication_Title
+          Authors
+          Publication_Year
+          Publication_Date
+          Publication
+          URL
+          DOI
+        }
+      }
+    }
+  }
+`
 
 export default PublicationsPage
