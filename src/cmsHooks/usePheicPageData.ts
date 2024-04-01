dbdab22f2b70d241a953dcc80233a9e95ac253c1
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2024-04-01 10:53:00 -0300

    CMS query for pheic cms data

---
+++ src/cmsHooks/usePheicPageData.ts
@@ -0,0 +1,26 @@
+import { useStaticQuery, graphql } from 'gatsby'
+
+import { AirtableCMSData } from 'components/library/airtable-cms/'
+
+// Sites will have many of these content hooks, each
+// of which corresponds to one table in Airtable.
+const usePheicPageData = () => {
+  const { cmsContent }: { cmsContent: AirtableCMSData } = useStaticQuery(
+    graphql`
+      query {
+        cmsContent: allAirtable(filter: { table: { eq: "PHEIC CMS" } }) {
+          nodes {
+            data {
+              Name
+              Text
+            }
+          }
+        }
+      }
+    `
+  )
+
+  return cmsContent
+}
+
+export default usePheicPageData
