046ea642639cd76cc18b4b7a9cb46bd72c6c9822
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2024-04-05 18:32:01 -0300

    Adding styling for map controls

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,14 +1,43 @@
 import React, { useState } from 'react'
+import styled from 'styled-components'
 import CMS from 'components/library/airtable-cms'
 
 import Providers from 'components/layout/Providers'
 import NavBar from 'components/layout/NavBar/NavBar'
 import FundingMap, { MapType } from 'components/map/Map'
 
+const MapControlContainer = styled.div`
+  position: absolute;
+  top: 100px;
+  right: 30px;
+  border-radius: 5px;
+  background-color: ${({ theme }) => theme.common.colors.surfaceGray100};
+  padding: 5px;
+  display: flex;
+  gap: 5px;
+`
+const MapControlButton = styled.button<{ selected?: boolean }>`
+  border: none;
+  background: none;
+  padding: 5px 10px;
+  border-radius: 5px;
+  transition: 200ms;
+
+  ${({ theme }) => theme.textStyleParagraph}
+
+  background-color: ${({ theme, selected }) =>
+    selected
+      ? theme.common.colors.surfaceThemeDarker
+      : theme.common.colors.surfaceGray100};
+
+  color: ${({ theme, selected }) =>
+    selected
+      ? theme.common.colors.textInvert
+      : theme.common.colors.textPrimary};
+`
+
 const GlobalPage = () => {
   const [mapType, setMapType] = useState<MapType>(MapType.Disbursed)
-
-  console.log({ setMapType })
 
   return (
     <Providers>
@@ -17,16 +46,21 @@ const GlobalPage = () => {
         description="Overall maps and charts for all data collected by GHS Tracking."
       />
       <NavBar />
-      <button
-        onClick={() =>
-          setMapType(prev =>
-            prev === MapType.Disbursed ? MapType.Recieved : MapType.Disbursed
-          )
-        }
-      >
-        Toggle map type
-      </button>
       <FundingMap mapType={mapType} interactive fullscreen />
+      <MapControlContainer>
+        <MapControlButton
+          selected={mapType === MapType.Disbursed}
+          onClick={() => setMapType(MapType.Disbursed)}
+        >
+          Funder
+        </MapControlButton>
+        <MapControlButton
+          selected={mapType === MapType.Recieved}
+          onClick={() => setMapType(MapType.Recieved)}
+        >
+          Recipient
+        </MapControlButton>
+      </MapControlContainer>
     </Providers>
   )
 }
