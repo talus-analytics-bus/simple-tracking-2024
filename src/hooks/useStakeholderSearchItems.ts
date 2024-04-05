5118b457ea44546ba6d084900e03eef9ff22ef28
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2024-04-05 19:26:10 -0300

    Adding pheics into the general stakeholder search

--- src/hooks/useStakeholderSearchItems.ts
+++ src/hooks/useStakeholderSearchItems.ts
@@ -1,19 +1,26 @@
+import usePheicNames from 'queryHooks/usePheicNames'
 import useStakeholders from 'queryHooks/useStakeholders'
 import { useMemo } from 'react'
 import simplifyForUrl from 'utilities/simplifyForUrl'
 
 const useStakeholderSearchItems = () => {
   const stakeholders = useStakeholders()
+  const pheics = usePheicNames()
 
   const items = useMemo(
     () =>
-      stakeholders
-        .map(stakeholder => ({
+      [
+        ...stakeholders.map(stakeholder => ({
           key: stakeholder.name ?? '',
           label: stakeholder.name ?? '',
           url: `/${stakeholder.slug}/${simplifyForUrl(stakeholder.name ?? '')}`,
-        }))
-        .sort((a, b) => a.label.localeCompare(b.label)),
+        })),
+        ...pheics.map(pheic => ({
+          key: pheic.data?.PHEIC_name ?? '',
+          label: pheic.data?.PHEIC_name ?? '',
+          url: `/pheic/${simplifyForUrl(pheic.data?.PHEIC_name ?? '')}`,
+        })),
+      ].sort((a, b) => a.label.localeCompare(b.label)),
     []
   )
 
