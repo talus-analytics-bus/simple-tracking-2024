63408c0ac0fadb99beca6409d4ace5772d3151b5
Ryan Zimmerman <rzimmerman@talusanalytics.com>
2024-04-05 18:57:56 -0300

    cleanip

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -60,7 +60,6 @@ const FundingMap = ({
     'source-layer': 'countries_v13c-6uk894',
     paint: {
       'line-color': theme.common.colors.strokeWhite,
-      // 'rgb(65, 101, 131)',
       'line-width': 2,
     },
     beforeId: 'country-label',
